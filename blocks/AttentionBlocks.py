25d2906934db33d18ee36abe40a1037157ae49f8
Louis CLeriot <louis.cleriot@telecomnancy.net>
2024-04-22 10:35:55 -0400

    SE and efficient channels attention blocks

--- blocks/AttentionBlocks.py
+++ blocks/AttentionBlocks.py
@@ -10,7 +10,7 @@ class SqueezeAndExciteBlock(nn.Module):
     sigmoid activation to the output. It multiplies the input by the output.
     """
     
-    def __init__(self,in_channels,downsample):
+    def __init__(self,in_channels,downsample=2):
         super(SqueezeAndExciteBlock,self).__init__()
         median_channels = in_channels // downsample
         self.fully_connected_1 = nn.Linear(in_channels, median_channels)
@@ -27,4 +27,31 @@ class SqueezeAndExciteBlock(nn.Module):
         output = output.view(output.size(0), -1, 1, 1)
         output = x * output
         output = torch.cat((x, output), 1)
+        return output
+    
+class EfficientChannelAttention(nn.Module):
+    """
+    same principle as the squeeze and excitation block but with a more efficient
+    implementation. It takes an input with in_channels, transform it into a 1d tensor
+    and apply 1 conv1d layer with a sigmoid activation, the result is the multiplied by the input.   	
+    https://doi.org/10.48550/arXiv.1910.03151
+    """
+    
+    def __init__(self,in_channels):
+        super(EfficientChannelAttention,self).__init__()
+        self.attentionlayer = nn.Conv1d(1,1,kernel_size=5,stride=1,padding=2)
+        
+    def forward(self,x):
+        #global average pooling
+        output = F.adaptive_avg_pool2d(x, (1,1))
+        #transform into 1d tensor
+        output = output.view(output.size(0), -1)
+        output = output.unsqueeze(1)
+        #go through the conv1d layer 
+        output = self.attentionlayer(output)
+        #apply sigmoid to get the attention weights
+        output = F.sigmoid(output)
+        #modulate the input with the attention weights
+        output = output.view(output.size(0), -1, 1, 1)
+        output = x * output
         return output
