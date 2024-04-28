028bdb7db7d7b8f8ddc44535c5f3356300b8f97b
Louis CLeriot <louis.cleriot@telecomnancy.net>
2024-04-28 11:40:12 -0400

    add slim convolution

--- blocks/ChannelsReduction.py
+++ blocks/ChannelsReduction.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+#import attention blocks
+from blocks.AttentionBlocks import SqueezeAndExciteBlock, EfficientChannelAttention
 class FireBlock(nn.Module):
     """ 
     takes an input with in_channels and applies a squeeze layer 
@@ -37,4 +39,62 @@ class FireBlock(nn.Module):
         expand1 = self.expand1(squeeze)
         expand2 = self.expand2(squeeze)
         output = torch.cat((expand1,expand2),1)
+        return output
+    
+class SlimConv(nn.Module):
+    """Reduce and reform feature channels to improve the quality of feature representations.
+    The SlimConv reduces calculations and maintains the capability of feature representations.
+    It's composed of an attention mechanism, 2 branches of convolutions, and a concatenation.
+    the first branch is a 3x3 convolution, the second is a 1x1 followed by a 3x3 convolution.
+    https://ieeexplore.ieee.org/document/9477103
+    
+    Args:
+        attentionMecanism (string): can be 'SE', 'ECA', default is SE
+        in_channels (int): the number of input channels
+        (Kup,Klow) (tuple of int): the reduction ratio for each branch, the default is (2,4)
+        downsample : use if SE corresponds to the reduction of the 2nd fc layer
+        gamma : use if ECA corresponds to the gamma value (mapping function)
+        beta : use if ECA corresponds to the beta value (mapping function)
+    """
+    def __init__(self, in_channels, attentionMecanism='SE', k=(2,4), downsample=2, gamma=2, beta=1):
+        super(SlimConv, self).__init__()
+        kup = k[0]
+        klow = k[1]
+        
+        if attentionMecanism == 'SE':
+            median_channels = in_channels // downsample
+            self.attention = nn.Sequential(
+                nn.AdaptiveAvgPool2d(1),
+                nn.Linear(in_channels, median_channels),
+                nn.ReLU(),
+                nn.Linear(median_channels, in_channels),
+                nn.Sigmoid()
+            )
+        else  :
+            t = int(abs((torch.log2(torch.tensor(in_channels, dtype=torch.float32)) + beta) / gamma))
+            kernel = t if t % 2 != 0 else t + 1
+            self.attention = nn.Sequential(
+                nn.AdaptiveMaxPool2d(1),
+                nn.Conv1d(1,1,kernel_size=kernel,stride=1,padding=kernel//2, bias=False),
+            )
+        self.upperBranch = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels//2, out_channels=in_channels//kup, kernel_size=3, stride=1, padding=1),
+        )
+        self.lowerBranch = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels//2, out_channels=in_channels//klow, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(in_channels//klow),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(in_channels=in_channels//klow, out_channels=in_channels//klow, kernel_size=3, stride=1, padding=1),
+        )
+        self.BN = nn.BatchNorm2d(in_channels//kup + in_channels//klow) 
+    def forward(self, x):
+        #calculate the attention weights
+        attention = self.attention(x)
+        attentionFlip = torch.flip(attention, [1])
+        #modulate the input with the attention weights
+        xupper = x * attention
+        xlower = x * attentionFlip
+        upper = self.upperBranch(xupper)
+        lower = self.lowerBranch(xlower)
+        output = self.BN(torch.cat((upper, lower), 1))
         return output
