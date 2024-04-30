3a11fd808784e803b56be064038c3f534361dea5
Louis CLeriot <louis.cleriot@telecomnancy.net>
2024-04-30 07:47:15 -0400

    add script to download dataset

--- datasets/get_FASHION8MNIST.sh
+++ datasets/get_FASHION8MNIST.sh
@@ -8,13 +8,3 @@ wget -c http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-
 wget -c http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz -P fashion-mnist 
 wget -c http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz -P fashion-mnist 
 wget -c http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz -P fashion-mnist 
-
-#unzip the files
-gunzip fashion-mnist/train-images-idx3-ubyte.gz
-gunzip fashion-mnist/train-labels-idx1-ubyte.gz
-gunzip fashion-mnist/t10k-images-idx3-ubyte.gz
-gunzip fashion-mnist/t10k-labels-idx1-ubyte.gz
-
-# remove the .gz files
-rm fashion-mnist/*.gz
-
