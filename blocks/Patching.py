b31d5546e8a3a74f657617455ef732fde3c4a89f
Louis CLeriot <louis.cleriot@telecomnancy.net>
2024-05-29 12:55:06 -0400

    refactor code

---
+++ blocks/Patching.py
@@ -0,0 +1,167 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class PatchEmbedding(nn.Module):
+    """PatchEmbedding is a module that splits the input image into patches and
+    then embeds the patches into linear vectors. The module can be used either
+    with a fully connected layer or a convolutional layer. For the patching 
+    part we use torch unfold function.
+    Args:
+        image_size (int or tuple): The size of the input image.
+        patch_size (int or tuple): The size of the patch.
+        in_channels (int): The number of channels in the input image.
+        embed_dim (int): The dimension of the output embeddings.
+        classifier_token (bool): Whether to add a classification token to the input embeddings.
+    """
+    def __init__(self,img_size, patch_size, in_channels, embed_dim, cls_token=True):
+        super(PatchEmbedding, self).__init__()
+        
+        if isinstance(patch_size, int):
+            patch_size = (patch_size, patch_size)
+        if isinstance(img_size, int):
+            img_size = (img_size, img_size)
+            
+        patchWidth, patchHeight = patch_size
+        imgWidth, imgHeight = img_size
+        numberOfPatches = (imgWidth*imgHeight)//(patchWidth*patchHeight)
+        
+        self.unfold = nn.Unfold(kernel_size=(patchWidth, patchHeight), stride=(patchWidth, patchHeight))
+        self.linear = nn.Linear(patchWidth*patchHeight*in_channels, embed_dim)
+
+        if cls_token:
+            self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
+            numberOfPatches += 1
+        else:
+            self.cls_token = None
+        
+        self.pos_embedding = nn.Parameter(torch.randn(1,numberOfPatches,embed_dim))
+        
+    def forward(self, x):
+        x = self.unfold(x)
+        x = x.permute(0,2,1)
+        x = self.linear(x)
+        
+        if self.cls_token is not None:
+            cls_token = self.cls_token.expand(x.size(0), -1, -1)
+            x = torch.cat((cls_token, x), dim=1)
+        
+        # Add positional embedding
+        x += self.pos_embedding
+        
+        return x
+
+class PatchEmbeddingConv(nn.Module):
+    """PatchEmbeddingConv is a module create the patch embeddings using a convolutional
+    layer. The module first applies a convolutional layer to the input image with a kernel
+    and stride size equal to the patch size, and an output channel equal to the dimension of
+    the embeddings. The output of the convolutional layer is then reshaped to create the patch
+    embeddings. The module can also add a classification token to the input embeddings.
+
+    Args:
+        image_size (int or tuple): The size of the input image.
+        patch_size (int or tuple): The size of the patch.
+        in_channels (int): The number of channels in the input image.
+        embed_dim (int): The dimension of the output embeddings.
+        classifier_token (bool): Whether to add a classification token to the input embeddings. 
+    """
+    def __init__(self, img_size, patch_size, in_channels, embed_dim, cls_token=True):
+        super(PatchEmbeddingConv, self).__init__()
+        
+        if isinstance(patch_size, int):
+            patch_size = (patch_size, patch_size)
+        if isinstance(img_size, int):
+            img_size = (img_size, img_size)
+            
+        patchWidth, patchHeight = patch_size
+        imgWidth, imgHeight = img_size
+        numberOfPatches = (imgWidth//patchWidth)*(imgHeight//patchHeight)
+        
+        self.conv = nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size)
+        
+        if cls_token:
+            self.cls_token = nn.Parameter(torch.randn(1,1,embed_dim))
+            numberOfPatches += 1
+        else:
+            self.cls_token = None
+        
+        self.pos_embedding = nn.Parameter(torch.randn(1,numberOfPatches,embed_dim))
+        
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.permute(0,2,3,1)
+        x = x.reshape(x.size(0), -1, x.size(-1))
+        
+        if self.cls_token is not None:
+            cls_token = self.cls_token.expand(x.size(0), -1, -1)
+            x = torch.cat((cls_token, x), dim=1)
+        
+        # Add positional embedding
+        x += self.pos_embedding
+        
+        return x
+    
+class ConvolutionalTokenEmbeding(nn.Module):
+    """Module use in Convolutional Vision Transformer to process 
+    a 2D image or a reshaped tensor into tokens map with new dimension
+    (B,C,H,W) using convolutional layer. We then reshape the output tensor
+    into a 1D tensor with shape (B, C, new_H * new_W) if the flag is set to true,
+    which is not use in CvT and normalize the tensor using layer normalization.
+
+    Args:
+        input_channels (int): The number of channels in the input image.
+        output_channels (int): The number of channels in the output tensor.
+        kernel_size (int or tuple): The size of the convolutional kernel.
+        stride (int or tuple): The stride of the convolutional kernel.
+        padding (int or tuple): The padding of the convolutional kernel.
+        reshape (bool): Whether to reshape the output tensor or not.
+    """
+    def __init__(self, input_channels, output_channels, kernel_size, stride, padding, reshape=False):
+        super(ConvolutionalTokenEmbeding, self).__init__()
+        
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        if isinstance(stride, int):
+            stride = (stride, stride)
+        if isinstance(padding, int):
+            padding = (padding, padding)
+        
+        self.conv = nn.Conv2d(input_channels, output_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=False)
+        self.norm = nn.LayerNorm(output_channels)
+        self.reshape = reshape 
+        
+    def forward(self, x):
+        B, C, H, W = x.size()
+        x = self.conv(x)
+        B, C, new_H, new_W = x.size()
+        if self.reshape:
+            x = x.view(B, new_H * new_W, C)
+            x = self.norm(x)
+            x = x.permute(0, 2, 1)
+        else:
+            x = x.permute(0, 2, 3, 1)
+            x = self.norm(x)
+            x = x.permute(0, 3, 1, 2)
+        return x
+    
+class ConvMixerPatchEmbeding(nn.Module):
+    """Module use in ConvMixer to tokenise patches of the input image. The module
+    applies a convolutional layer to the input image with a kernel and stride
+    size equal to the patch size, and an output channel equal to the dimension of the embeddings.
+    
+    Args:
+        in_channels (int): The number of channels in the input image.
+        embed_dim (int): The dimension of the output embeddings.
+        patch_size (int or tuple): The size of the patch.
+    """
+    
+    def __init__(self, in_channels, embed_dim, patch_size):
+        super(ConvMixerPatchEmbeding, self).__init__()
+        
+        self.Patching =nn.Sequential(
+            nn.Conv2d(in_channels, embed_dim, kernel_size=patch_size, stride=patch_size),
+            nn.GELU(),
+            nn.BatchNorm2d(embed_dim))
+        
+    def forward(self, x):
+        return self.Patching(x)
---
+++ blocks/Transformer.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from blocks.BasicBlocks import ResAdd
+from blocks.Patching import PatchEmbeddingConv
+from blocks.AttentionBlocks import MultiHeadAttention
+""" 
+TODO : 
+
+- CvT : Convolutional Vision Transformer
+- LeViT : Lightweight and Efficient Vision Transformer
+- BoTNet : Bottleneck Transformers
+- MobileViT : Mobile Vision Transformer
+- ViLT
+"""
+        
+        
+        
+class VisionTransformer(nn.Module):
+    """Vision Transformer is the adaptation of the Transformer model to the
+    computer vision domain. It begin by splitting the input image into patches,
+    using a feedforward neural network to embed the patches into linear vectors,
+    and then feeding these vectors into a standard Transformer model. To this vector
+    we concatenate a learnable positional encoding to the input embeddings and add 
+    a classification token to the input embeddings. The classification token is used
+    to aggregate information from the output of the Transformer model and make a prediction.
+    At the output of the Transformer model, we apply a linear layer to the output of the
+    classification token to make a prediction. 
+    Args:
+        image_size (int or tuple): The size of the input image.
+        patch_size (int or tuple): The size of the patch.
+        num_classes (int): The number of classes in the dataset.
+        dim (int): The dimension of the input embeddings.        
+    """
+    def __init__(self, image_size, patch_size, in_channels, embed_dim, L, heads, n_class, cls_token=True, dk=None, dv=None):
+        super(VisionTransformer, self).__init__()
+        self.patch_embedding = PatchEmbeddingConv(image_size, patch_size, in_channels, embed_dim, cls_token)
+        
+        encoder = nn.ModuleList()
+        for _ in range(L):
+            module1 = nn.Sequential(
+                nn.LayerNorm(embed_dim),
+                MultiHeadAttention(dmodel=embed_dim,heads=heads, dk=dk, dv=dv)
+                )
+            encoder.append(nn.Sequential(ResAdd(module1)))
+            module2 = nn.Sequential(
+                nn.LayerNorm(embed_dim),
+                nn.Linear(embed_dim, embed_dim),
+                nn.GELU(),
+                nn.Linear(embed_dim, embed_dim),
+                nn.GELU()
+                )
+            encoder.append(nn.Sequential(ResAdd(module2)))
+        self.encoder = nn.Sequential(*encoder)
+        self.MLPHead = nn.Sequential(
+            nn.LayerNorm(embed_dim),
+            nn.Linear(embed_dim, (embed_dim+n_class)//2),
+            nn.ReLU(),
+            nn.Linear((embed_dim+n_class)//2, n_class)
+            )
+    
+    def forward(self, x):
+        x = self.patch_embedding(x)
+        x = self.encoder(x)
+        x = x[:,0]
+        x = self.MLPHead(x)
+        return x
+
+from blocks.Patching import ConvolutionalTokenEmbeding
+from blocks.AttentionBlocks import ConvProjectionLayer
+            
+class ConvolutionalVisionTransformerStage(nn.Module):
+    """Convolutional Vision Transformer is the adaptation of the Vision Transformer model using 
+    convolutional layers to tokenise patches of the input image. The ConvProjectionLayer
+    will use this tokenised patches to compute the attention weights using spatial information
+    and the MultiHeadAttention mechanism. Like other Transformer models, we use a classification
+    token to aggregate information from the output of the Transformer model and make a prediction.
+    at the end of the model with a MLPHead.
+    https://arxiv.org/pdf/2103.15808
+    
+    Args:
+        patch_in_channels (int): The number of channels in the input patches.
+        patch_out_channels (int): The number of channels in the output patches.
+        patch_kernel_size (int): The size of the kernel in the convolutional layer.
+        patch_stride (int): The stride of the convolutional layer.
+        MH_heads (int): The number of heads in the MultiHeadAttention mechanism.
+        expand_channels (int): The factor by which to expand the number of channels in the MLP of the convprojection.
+        n_ConvProjection (int): The number of ConvProjectionLayer in this stage.
+        squeeze_factor (int): The factor by which to reduce the number of channels in the convolutional layer.
+        cls_token (bool): Whether to add a classification token to the input embeddings.
+        nb_classes (int): The number of classes in the dataset. 
+    """
+    
+    def __init__(self, patch_in_channels, patch_out_channels, patch_kernel_size, patch_stride, MH_heads, expand_channels, n_ConvProjection, squeeze_factor=2, cls_token=False, nb_classes=1000):
+        super(ConvolutionalVisionTransformerStage, self).__init__()
+        
+        padding = patch_kernel_size // 2
+        self.patch_embedding = ConvolutionalTokenEmbeding(patch_in_channels, patch_out_channels, patch_kernel_size, patch_stride, padding)
+        conv_projection = nn.ModuleList()
+        last = False
+        for i in range(n_ConvProjection):
+            if cls_token:
+                last = i == n_ConvProjection - 1
+            conv_projection.append(
+                ConvProjectionLayer(patch_out_channels, patch_out_channels, 3, expand_channels, MH_heads, squeeze_factor, cls_token, last)
+                )
+        if cls_token:
+            mlp = nn.Sequential(
+                nn.LayerNorm(patch_out_channels),
+                nn.Linear(patch_out_channels, patch_out_channels * expand_channels),
+                nn.GELU(),
+                nn.Linear(patch_out_channels * expand_channels, nb_classes)
+                )
+            conv_projection.append(mlp)
+        self.conv_projection = nn.Sequential(*conv_projection)
+        
+    def forward(self, x):
+        x = self.patch_embedding(x)
+        x = self.conv_projection(x)
+        return x
+            
